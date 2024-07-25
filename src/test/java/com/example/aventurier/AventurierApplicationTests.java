09daa0268531cbdb27717430e1c17c39b6fd08bc
YounesAkel <younes.akel@e-polytechnique.com>
2024-07-25 14:02:11 +0100

    test fixing

---
+++ src/test/java/com/example/aventurier/AventurierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aventurier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AventurierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
